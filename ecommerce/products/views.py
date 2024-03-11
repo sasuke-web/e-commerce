1401c2db0241ac28080c90e3ba716ab991bc94e1
Karan Gosalia <karangosalia15@gmail.com>
2024-03-11 23:25:46 +0530

    Set products app

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
