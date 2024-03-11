ffe31ea977b1f75cecfd6d04b2dc1fd1287f66df
Karan Gosalia <karangosalia15@gmail.com>
2024-03-11 23:17:05 +0530

    Set customers app

---
+++ ecommerce/customers/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Customer
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'name', 'contact_number', 'email']
+        extra_kwargs = {
+            'name': {'validators': []},  # Disable default unique validator
+        }
+
+    def validate_name(self, value):
+        if self.instance is not None and self.instance.name == value:
+            return value
+        if Customer.objects.filter(name=value).exists():
+            raise serializers.ValidationError("A customer with this name already exists.")
+        return value
--- ecommerce/customers/views.py
+++ ecommerce/customers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your views here.
+class CustomerListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class CustomerRetrieveUpdateAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
